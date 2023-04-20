908d4180d5980d37e8d080d9a9a29ca3bddba729
Alex Lirola Fenoy <alirola-@c1r13s6.42malaga.com>
2023-04-20 13:41:20 +0200

    add files

---
+++ Makefile
@@ -0,0 +1,32 @@
+NAME = libft.a
+
+FLAGS = -Wall -Werror -Wextra
+
+FILES = ft_isalpha.c\
+		ft_isdigit.c\
+		ft_isalnum.c\
+		ft_isascii.c\
+		ft_isprint.c\
+		ft_strlen.c\
+		ft_toupper.c\
+		ft_tolower.c\
+
+OBJS = $(FILES:.c=.o)
+
+all : $(NAME)
+
+$(NAME) : $(OBJS)
+	ar rcs $(NAME) $(OBJS)
+
+$(OBJS) : $(FILES)
+	gcc $(FLAGS) -c $(FILES)
+
+clean:
+	rm -f $(OBJS)
+	
+fclean:
+	rm -f $(NAME) $(OBJS)
+
+re : all
+
+.PHONY : all re fclean clean
---
+++ libft.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: alirola- <alirola-@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/20 13:24:38 by alirola-          #+#    #+#             */
+/*   Updated: 2023/04/20 13:40:25 by alirola-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+# include <stdlib.h>
+
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *str);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+int		ft_isascii(int c);
+
+#endif 
