4ecce2b84f79e4f0cdc3b75e9396516c8c067556
Alex Lirola Fenoy <alirola-@c2r1s4.42malaga.com>
2023-04-26 12:54:33 +0200

    primera pagina terminada

--- Makefile
+++ Makefile
@@ -24,6 +24,7 @@ FILES = ft_isalpha.c\
 		ft_strnstr.c\
 		ft_atoi.c\
 		ft_calloc.c\
+		ft_strdup.c\
 
 OBJS = $(FILES:.c=.o)
 
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: alirola- <alirola-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/20 13:24:38 by alirola-          #+#    #+#             */
-/*   Updated: 2023/04/25 18:33:03 by alirola-         ###   ########.fr       */
+/*   Updated: 2023/04/26 11:47:55 by alirola-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ int		ft_memcmp(const void *s1, const void *s2, size_t n);
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
+char	*ft_strdup(const char *s1);
 
 #endif 
